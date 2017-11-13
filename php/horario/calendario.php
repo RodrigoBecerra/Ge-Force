3207d18b0467b173f6a2da7bd5d521a628eae3e3
miguelkjkszpj <miguel.torres2016@alu.uct.cl>
2017-11-13 14:55:22 -0300

    calendario
    iframe faltante

---
+++ php/horario/calendario.php
@@ -0,0 +1,1 @@
+<iframe src="https://calendar.google.com/calendar/embed?showTitle=0&amp;showPrint=0&amp;showTabs=0&amp;showTz=0&amp;height=260&amp;wkst=1&amp;bgcolor=%233366ff&amp;src=rro6u48523urstq3i44mk34dag%40group.calendar.google.com&amp;color=%2329527A&amp;ctz=America%2FSantiago" style="border-width:0" width="320" height="260" frameborder="0" scrolling="no"></iframe>
