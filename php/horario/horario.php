5ef62e62714563dad4f7d0db3d0cf79daabb9f8e
miguelkjkszpj <miguel.torres2016@alu.uct.cl>
2017-11-14 14:22:21 -0300

    .

--- php/horario/horario.php
+++ php/horario/horario.php
@@ -13,20 +13,23 @@ var sala = new Array(cuenta);
 var dia = new Array(cuenta);
 var año = new Array(cuenta);
 var seccion = new Array(cuenta);
+var id_Pl = new Array(cuenta);
 console.log('$cuenta');
 ";
-while ($registro = $consulta->fetch_object()){  
+while ($registro = $consulta->fetch_object()){
 	//$nombre = ucfirst(strtolower($registro->nombre)); para usar en el futuro en los nombres
 	$hora = $registro->Hora ? $registro->Hora : "no hay hora disponible";
 	$sala = $registro->Sala ? $registro->Sala : "no hay sala";
 	$dia = $registro->Dia ? $registro->Dia : "no hay dia disponible";
 	$año = $registro->A ? $registro->A : "no hay año disponible";
 	$seccion = $registro->Seccion ? $registro->Seccion : "no hay Seccion disponible";
+	$Id_Pl = $registro->Id_Pl ? $registro->Id_Pl : "no hay id_Pl";
 	echo"hora[$cuent] = '$hora';
 	sala[$cuent] = '$sala';
 	dia[$cuent] = '$dia';
 	año[$cuent] = '$año';
-	seccion[$cuent] = '$seccion';";
+	seccion[$cuent] = '$seccion';
+	id_Pl[$cuent] = '$Id_Pl';";
 
 
 	//solo se muestra para ver todo en caso de necesitarlo
