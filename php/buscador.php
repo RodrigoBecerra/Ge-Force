64b0fd763a732749759e60a9c1cb2b33cffcc6f7
sdiazcerda <31453397+sdiazcerda@users.noreply.github.com>
2017-11-20 13:21:10 -0300

    Cambiando cosas pequeñas
    .

--- php/buscador.php
+++ php/buscador.php
@@ -6,19 +6,19 @@
 <body>
 <?php
 // Conectarse al motor de Base de Datos
-include("Conex.inc");
-$numEmp = $_GET["numEmp"];
+include("../conex.inc");
+$busca=$_GET["T1"];
 
 //Hacer la consulta
-$consulta = "Select * FROM empleadosk17 
-            WHERE NoEmpleado = $numEmp";
+$consulta = "Select * FROM Asignatura
+            WHERE Asignatura OR Profesor OR Repeticion LIKE $busca";
 $respuesta = mysqli_query($db, $consulta);
 if(mysqli_num_rows($respuesta)==0) echo "Registro No encontrado";
 
 if($respuesta)
   //Mostramos la respuesta
-  while($fila = mysqli_fetch_object($respuesta))
-    echo "$fila->Nombre $fila->Apellido $fila->Sexo $fila->FechaNacimiento $fila->Sueldo $fila->Departamento $fila->descripcion <br>";
+while($fila = mysqli_fetch_object($respuesta))
+   echo "$fila->Nombre $fila->Apellido $fila->Sexo $fila->FechaNacimiento $fila->Sueldo $fila->Departamento $fila->descripcion <br>";
 else
   echo "Error!!!!";
 ?>
