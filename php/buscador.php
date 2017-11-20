656810ae7593f1cfdc1592cd1aba5ded2cc6e2a4
miguelkjkszpj <miguel.torres2016@alu.uct.cl>
2017-11-20 18:50:12 -0300

    Revert "Merge branch 'master' of https://github.com/RodrigoBecerra/Ge-Force"
    This reverts commit 15d60f6992170ffbce67ac45649eada1afa1eb5e, reversing
    changes made to cc504247677cd052e4a639c1d72f049cdd5744f0.

--- php/buscador.php
+++ php/buscador.php
@@ -2,8 +2,7 @@
 <html lang="es">
 <head>
 <meta charset="UTF-8">
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
 </head>
 <body>
 <?php
@@ -12,42 +11,18 @@ include("../conex.inc");
 $busca= $_POST["busca"];
 
 //Hacer la consulta
-$consulta2 = "SELECT * FROM asignatura as asig
-NATURAL JOIN horario as ho  WHERE asig.Asignatura LIKE ('%$busca%')";
 $consulta = "SELECT * FROM asignatura as asig
-NATURAL JOIN horario as ho  WHERE asig.Profesor LIKE ('%$busca%')";
-
+NATURAL JOIN horario as ho  WHERE Profesor LIKE ('%$busca%')";
 
 $respuesta = mysqli_query($db, $consulta);
-if(mysqli_num_rows($respuesta)==0)
-	echo "Registro no encontrado";
-
+if(mysqli_num_rows($respuesta)==0) echo "Registro no encontrado";
 
 if($respuesta){
   //Mostramos la respuesta
-	echo "<table class='table table-hover'>";
-	echo "<thead><tr><th>Codigo Asignatura</th> <th>Hora</th> <th>Sala</th>  <th>Seccion</th> <th>Asignatura</th> <th>Profesor</th> <th>Nivel</th> <th>Dia</th> <th>Repeticion</th></tr></thead><tbody>";
-	while($fila = mysqli_fetch_object($respuesta)){
-    	echo "<tr><th>$fila->Id_As</th> <th>$fila->Hora</th> <th>$fila->Sala</th> <th>$fila->Id_Se</th> <th>$fila->Asignatura</th><th>$fila->Profesor</th> <th>$fila->A</th> <th>$fila->Dia</th> <th>$fila->Repeticion</th></tr>";
-    }
-
-	echo "</tbody></table>";
-}
-else{
-	echo "No se encontro el resultado.";
-}
-
-
-$respuesta2 = mysqli_query($db, $consulta2);
-if(mysqli_num_rows($respuesta)==0)
-	echo "Registro no encontrado";
-
-if($respuesta2){
-  //Mostramos la respuesta
 	echo "<table class='table'>";
-	echo "<thead><tr><th>Codigo Asignatura</th> <th>Hora</th> <th>Seccion</th> <th>Asignatura</th> <th>Profesor</th> <th>Nivel</th> <th>Dia</th> <th>Repeticion</th></tr></thead><tbody>";
-	while($fila = mysqli_fetch_object($respuesta2)){
-    	echo "<tr><th>$fila->Id_As</th> <th>$fila->Hora</th> <th>$fila->Id_Se</th> <th>$fila->Asignatura</th><th>$fila->Profesor</th> <th>$fila->A</th> <th>$fila->Dia</th> <th>$fila->Repeticion</th></tr>";
+	echo "<thead><tr><th>Codigo Asignatura</th> <th>Seccion</th> <th>Asignatura</th> <th>Profesor</th><th>Repeticion</th></tr></thead><tbody>";
+	while($fila = mysqli_fetch_object($respuesta)){
+    	echo "<tr><th>$fila->Id_As</th> <th>$fila->Id_Se</th> <th>$fila->Asignatura</th> <th>$fila->Profesor</th> <th>$fila->Repeticion</th></tr>";
     }
 
 	echo "</tbody></table>";
@@ -56,6 +31,7 @@ else{
 	echo "No se encontro el resultado.";
 }
 ?>
-<button class="btn btn-default"><a href="indexPC.php">Volver a la pagina principal</a></button>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+ <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 </body>
 </html>
