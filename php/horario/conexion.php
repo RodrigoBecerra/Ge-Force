4910454c918094fffdec84e61f7f69f1bcaa9f05
Nicolas Rivas <nrivas2017@alu.uct.cl>
2017-11-20 13:57:10 -0300

    cambio

--- php/horario/conexion.php
+++ php/horario/conexion.php
@@ -30,7 +30,7 @@ class DB {
     }
 
     public static function gethorario(){
-        $consulta_resultados = DB::pQRY("SELECT * FROM `horario` ORDER BY `id_Ho`")
+        $consulta_resultados = DB::pQRY("SELECT * FROM `horario` ORDER BY `id_Horario`")
             or die("No se encontro");
         return $consulta_resultados;
     }
