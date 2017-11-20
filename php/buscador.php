6735b842745c4aae21d050e521e504ae92c75414
sdiazcerda <31453397+sdiazcerda@users.noreply.github.com>
2017-11-20 14:58:30 -0300

    Tabla completa con bootstrap
    lista, faltan mas consultas que serán realizadas hoy.

--- php/buscador.php
+++ php/buscador.php
@@ -2,6 +2,7 @@
 <html lang="es">
 <head>
 <meta charset="UTF-8">
+
 </head>
 <body>
 <?php
@@ -10,17 +11,27 @@ include("../conex.inc");
 $busca= $_POST["busca"];
 
 //Hacer la consulta
-$consulta = "Select * FROM asignatura WHERE Profesor LIKE ('%$busca%')";
+$consulta = "SELECT * FROM asignatura as asig
+NATURAL JOIN horario as ho  WHERE Profesor LIKE ('%$busca%')";
 
 $respuesta = mysqli_query($db, $consulta);
 if(mysqli_num_rows($respuesta)==0) echo "Registro no encontrado";
 
-if($respuesta)
+if($respuesta){
   //Mostramos la respuesta
-	while($fila = mysqli_fetch_object($respuesta))
-    	echo "$fila->Id_As $fila->Id_Se $fila->Asignatura $fila->Profesor $fila->Repeticion<br>";
-else
-  echo "Error!!!!";
+	echo "<table class='table'>";
+	echo "<thead><tr><th>Codigo Asignatura</th> <th>Seccion</th> <th>Asignatura</th> <th>Profesor</th><th>Repeticion</th></tr></thead><tbody>";
+	while($fila = mysqli_fetch_object($respuesta)){
+    	echo "<tr><th>$fila->Id_As</th> <th>$fila->Id_Se</th> <th>$fila->Asignatura</th> <th>$fila->Profesor</th> <th>$fila->Repeticion</th></tr>";
+    }
+
+	echo "</tbody></table>";
+}
+else{
+	echo "No se encontro el resultado.";
+}
 ?>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+ <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 </body>
 </html>
