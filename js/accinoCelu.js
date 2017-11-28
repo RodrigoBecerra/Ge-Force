53003cf3d5da34abe4129778f576e62c98ed9250
miguelkjkszpj <miguel.torres2016@alu.uct.cl>
2017-11-28 00:00:35 -0300

    celular 2.0

--- js/accinoCelu.js
+++ js/accinoCelu.js
@@ -1,42 +1,57 @@
 /*cambios para que funcione en celular ya que no tengo contemplado librerias css aca
  */
+ var horaIB = ["8:00","8:30","9:10","9:40","10:20","10:50","11:30","12:00","12:40","13:10","13:50","14:20","15:00","15:30","16:10","16:40","17:20","17:50","18:30","19:00","19:40","20:10","20:50","21:20"];
  var x = [0,0,0];
  var dia = "";
  var ppeed = "";//para poner en el div
  console.log("accinocelu");
 
  function mostrarhor(x,y,z){
- 	 console.log(diacls);
- 	ppeed = "<hr class='half-rule'/>";
+ 	ppeed = "";
    var dived = document.getElementById("ins"); //edicion del div
    console.log(x,"-",y);
+   proximacls(x,y);
    for(var i=0;i<cuenta;i++){
-   		if(String(año[i])==String(x) && String(seccion[i])==String(y)){
+   	if(seccion[i].length > 1){
+   		var sussec = seccion[i].split('-');
+   		for(var j=0;j<sussec.length;j++)
+   		if(String(año[i])==String(x) && String(sussec[j])==String(y)){
    			if(z==diacls[i]){
-   				ppeed = ppeed+"<br><p>Sala: "+sala[i]+"</p><br><p>Hora: "+hora[i]+"</p><br><p>Asignatura: "+asig[i]+"</p><br><p>Profesor: "+profe[i]+"</p><br><hr class='half-rule'/>";
+   				ppeed = ppeed+"<div class='col-xl-6 thumbnail' style='background-color:rgba(10,20,255,0.6)'> <p>Sala: "+sala[i]+"</p><br><p>Hora: "+hora[i]+"</p><br><p>Asignatura: "+asig[i]+"</p><br><p>Profesor: "+profe[i]+"</p></div><hr class='half-rule'/>";
    			}
    			else if(z=="Todos"){
-   				ppeed = ppeed+"<br><p>Sala: "+sala[i]+"</p><br><p>Dia: "+diacls[i]+"</p><br><p>Hora: "+hora[i]+"</p><br><p>Asignatura: "+asig[i]+"</p><br><p>Profesor: "+profe[i]+"</p><br><hr class='half-rule'/>";
+   				ppeed = ppeed+"<div class='col-xl-6 thumbnail' style='background-color:rgba(10,20,255,0.6)'><p>Sala: "+sala[i]+"</p><br><p>Dia: "+diacls[i]+"</p><br><p>Hora: "+hora[i]+"</p><br><p>Asignatura: "+asig[i]+"</p><br><p>Profesor: "+profe[i]+"</p></div><hr class='half-rule'/>";
    			}
    		}
+   	}
+   	else{
+   		if(String(año[i])==String(x) && String(seccion[i])==String(y)){
+   			if(z==diacls[i]){
+   				ppeed = ppeed+"<div class='col-xl-6 thumbnail' style='background-color:rgba(10,20,255,0.6)'><p>Sala: "+sala[i]+"</p><br><p>Hora: "+hora[i]+"</p><br><p>Asignatura: "+asig[i]+"</p><br><p>Profesor: "+profe[i]+"</p></div><hr class='half-rule'/>";
+   			}
+   			else if(z=="Todos"){
+   				ppeed = ppeed+"<div class='col-xl-6 thumbnail' style='background-color:rgba(10,20,255,0.6)'><p>Sala: "+sala[i]+"</p><br><p>Dia: "+diacls[i]+"</p><br><p>Hora: "+hora[i]+"</p><br><p>Asignatura: "+asig[i]+"</p><br><p>Profesor: "+profe[i]+"</p></div><hr class='half-rule'/>";
+   			}
+   		}
+   	}
    }
    dived.innerHTML=ppeed;
  }
 
  function dias(day){
- 	if (day==5){
+ 	if (day==5 || day=="Fri"){
  		return "Viernes";
  	}
- 	else if(day==4){
+ 	else if(day==4 || day=="Thu"){
  		return "Jueves";
  	}
- 	else if(day==3){
+ 	else if(day==3 || day=="Wed"){
  		return "Miercoles";
  	}
- 	else if(day==2){
+ 	else if(day==2 || day=="Tue"){
  		return "Martes";
  	}
- 	else if(day==1){
+ 	else if(day==1 || day=="Mon"){
  		return "Lunes";
  	}
  	else{
@@ -62,4 +77,24 @@
 	else{
 		document.getElementById("ti").innerHTML = "seleccione en el otro campo tambien.";
 	}
+ }
+
+ function proximacls(numa,nums){
+ 	var d = new Date();
+    var n = d.toString();
+    d = n.split(" ");
+    n = d[4].split(":");
+    d = dias(d[0]);
+    console.log(d);
+    var n2 = "";
+    if(d != "Todos"){
+    	for(var i=0;i<cuenta;i++){
+    		n2 = horaIB[i].split(":");
+    		if(n[0] <= n2[0] && d == diacls[i]){
+    			return "<hr class='half-rule'/><div class='col-xl-6 thumbnail' style='background-color:rgba(255,20,10,0.6)'><p>proxima clase de este dia:</p><br><p>Sala: "+sala[i]+"</p><br><p>Hora: "+hora[i]+"</p><br><p>Asignatura: "+asig[i]+"</p><br><p>Profesor: "+profe[i]+"</p></div><hr class='half-rule'/>";
+    		}
+    	}
+    }else{
+    	return "<hr class='half-rule'/>";
+    }
  }
