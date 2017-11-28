8a87b91d688b61af7786446a34b83b74f252d042
miguelkjkszpj <miguel.torres2016@alu.uct.cl>
2017-11-28 00:07:11 -0300

    discus apaña
    ahora va por año y seccion

--- php/disqus.php
+++ php/disqus.php
@@ -1,7 +1,8 @@
-
+<?php if(!empty($_GET['v'])){$vurld = $_GET['v'];}else{$vurld = "";}
+echo "
 <script>
 var disqus_config = function () {
-this.page.identifier = "comentario";
+this.page.identifier = 'comentario$vurld';
 };
 (function() {
 var d = document, s = d.createElement('script');
@@ -10,4 +11,5 @@ s.setAttribute('data-timestamp', +new Date());
 (d.head || d.body).appendChild(s);
 })();
 </script>
-<noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+<noscript>Please enable JavaScript to view the <a href='https://disqus.com/?ref_noscript'>comments powered by Disqus.</a></noscript>
+";
